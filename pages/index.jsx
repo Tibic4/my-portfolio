5aaa20f9d83ddb70f3c6049963e0995173c16600
Alton Vieira <bicagold@gmail.com>
2022-12-26 09:09:46 -0300

    feat: home page design

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,24 @@
+import Link from 'next/link';
 import Head from '../components/Head';
+import styles from '../styles/HomePage.module.css';
 
-export default function Home() {
+export default function HomePage() {
   return (
     <>
-      <Head title="Alton Vieira | Home" />
-      <h1>Home Page</h1>
+      <Head title="Nitin Ranganath | Home" />
+      <div className={styles.container}>
+        <h6 className={styles.welcome}>Olá, geeks! Eu sou</h6>
+        <h1 className={styles.title}>Alton Vieira</h1>
+        <h3 className={styles.subtitle}>&lt;!-- Web Developer --&gt;</h3>
+        <div className={styles.buttons}>
+          <Link href="/about">
+            <button className={styles.contained}>O que eu posso fazer?</button>
+          </Link>
+          <Link href="/contact">
+            <button className={styles.outlined}>Entre em Contato</button>
+          </Link>
+        </div>
+      </div>
     </>
   );
 }
